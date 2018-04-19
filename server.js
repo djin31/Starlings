8e0997396041d65139c913ff3bea096adf5e5239
ArpanMangal <mangalarpan@gmail.com>
2018-04-19 17:48:27 +0530

    Made a running express app

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+// Static Content
+app.use(express.static('static'));
+
+// Defining routes
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/templates/index.html');
+});
+
+app.listen(PORT, function () {
+    console.log('App running. Visit http://localhost:' + PORT);
+})
